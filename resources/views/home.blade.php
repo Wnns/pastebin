dec46c932d2d666735851b8eda1c9daff37d82d7
Wnns <wnnspro@gmail.com>
2017-03-04 02:09:56 +0100

    file structure changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,67 +1,17 @@
-@extends('master')
+@extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
 
-@foreach($errors->all() as $error)
-
-	<div class="alert alert-danger">
-		
-		{{$error}}
-	</div>
-@endforeach
-	
-	<h1>Create new paste</h1><br><br>
-	<form action="addPaste" method="post">
-		
-		<input type="hidden" name="_token" value="{{ csrf_token() }}">
-		<textarea class="form-control paste-textarea" name="pasteContent">{{ old('pasteContent')}}</textarea><br><br>
-		<div class="form-inline">
-
-			<label for="pasteTitle">Title</label>
-			<input type="text" name="pasteTitle" class="form-control" value="{{ old('pasteTitle', '') }}">
-		</div><br>
-		<div class="form-inline">
-
-			<label for="pasteAuthor">Author</label>
-			<input type="text" name="pasteAuthor" class="form-control" value="{{ old('pasteAuthor') }}">
-		</div><br>
-		<div class="form-inline">
-
-			<label for="pasteExpiryDate">Paste Expiration:</label>
-			<select class="form-control" name="pasteExpiryDate">
-				
-				<option>Never</option>
-				<option>5 minutes</option>
-				<option>30 minutes</option>
-				<option>1 hour</option>
-				<option>1 day</option>
-				<option>1 week</option>
-				<option>1 month</option>
-			</select>
-		</div><br>
-		<div class="form-inline">
-
-			<label for="pasteSyntaxHighlighting">Syntax highlighting:</label>
-			<select class="form-control" name="pasteSyntaxHighlighting">
-				
-				<option>None</option>
-				<option>C#</option>
-				<option>C++</option>
-				<option>CSS</option>
-				<option>HTML</option>
-				<option>JSON</option>
-				<option>Java</option>
-				<option>JavaScript</option>
-				<option>PHP</option>
-				<option>Python</option>
-				<option>Ruby</option>
-				<option>SQL</option>
-			</select>
-		</div><br>
-		<input type="checkbox" name="pasteIsPrivate" />
-		<abbr title="Paste will not be showed in Last pastes section">Private</abbr>
-		<br><br>
-		<input type="submit" class="btn btn-success" value="Create new paste">
-		<br><br><br>
-	</form>
-@stop
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/popular.blade.php
@@ -0,0 +1,33 @@
+@extends('master')
+
+@section('content')
+	<h1>Popular pastes</h1><br>
+	@if($popularPastes)
+	<table class="table table-striped">
+		
+		<thead class="thead-inverse">
+
+			<tr>
+
+				<td class="col-md-8">Title</td>
+				<td>Added</td>
+				<td>Views</td>
+				<td>Author</td>
+			</tr>
+		</thead>
+		<tbody>
+		@foreach($popularPastes as $pasteData)
+			<tr>
+				
+				<td><a href="{{ URL::to('/p/' . $pasteData['string_id']) }}">{{ $pasteData['title'] }} </a></td>
+				<td>{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $pasteData['created_at'])->diffForHumans() }} </td>
+				<td>{{ $pasteData['views'] }} </td>
+				<td>{{ $pasteData['author'] }} </td>
+			</tr>
+		@endforeach
+		</tbody>
+	</table>
+	@else
+	No pastes yet.
+	@endif
+@stop
